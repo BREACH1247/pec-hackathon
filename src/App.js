5ec45a0ef60acdff80aba68468a2697898866f97
Aditya Sinha <sinhaditya50@gmail.com>
2022-04-17 16:25:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Navbar from "./components/Footer and Navbar/navbar"
-import Input from "./components/Footer and Navbar/input";
-import 
+import Input from "./components/Footer and Navbar/input"; 
 function App() {
   return (
     <div class="main-app">
